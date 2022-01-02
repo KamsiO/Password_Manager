14c4ed48393adbc78a91bcbf5a9e513bf03db838
Kamsi Oramasionwu <50928548+KamsiO@users.noreply.github.com>
2022-01-02 20:40:07 +0100

    Update Password.java
    Added encryption capabilities

--- src/main/model/Password.java
+++ src/main/model/Password.java
@@ -124,6 +124,73 @@ public class Password {
             strengthRating = "Medium";
         }
     }
+    
+    // EFFECTS: encrypts the password string
+    public String encrypt() {
+        char[] string = pw.toCharArray();
+        StringBuilder encrypted = new StringBuilder();
+        int addVal = 1;
+        for (int i = 0; i < pw.length(); i++) {
+            if (isLetter(string[i])) {
+                char c;
+                if (isUpperCase(string[i])) {
+                    c = toLowerCase(string[i]);
+                } else {
+                    c = toUpperCase(string[i]);
+                }
+                encrypted.append(((int) c) + addVal);
+            } else {
+                encrypted.append(((int) string[i]) + addVal);
+            }
+            encrypted.append(" ");
+            if (addVal == 1) {
+                addVal = 0;
+            } else {
+                addVal = 1;
+            }
+        }
+        return encrypted.reverse().toString().trim();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: decrypts the password string and sets the password to decrypted string
+    public void decrypt() {
+        System.out.println("decrypt");
+        StringBuilder encrypted = new StringBuilder(pw);
+        StringBuilder temp = new StringBuilder();
+        encrypted.reverse();
+        int subVal = 0;
+        if (temp.length() % 2 == 0) {
+            subVal = 1;
+        }
+        String[] strings = encrypted.toString().split("\\s+");
+        List<Integer> asciis = new ArrayList<>();
+        for (String string : strings) {
+            asciis.add(Integer.valueOf(string));
+        }
+        for (int ascii : asciis) {
+            temp.append(Character.toString((char) (ascii - subVal)));
+            System.out.println(temp);
+            if (subVal == 1) {
+                subVal = 0;
+            } else {
+                subVal = 1;
+            }
+        }
+        StringBuilder decrypted = new StringBuilder();
+        for (int i = 0; i < temp.length(); i++) {
+            if (isLetter(temp.charAt(i))) {
+                if (isUpperCase(temp.charAt(i))) {
+                    decrypted.append(toLowerCase(temp.charAt(i)));
+                } else {
+                    decrypted.append(toUpperCase(temp.charAt(i)));
+                }
+            } else {
+                decrypted.append(temp.charAt(i));
+            }
+        }
+        setPassword(decrypted.toString());
+    }
 
     // EFFECTS: returns password's strength rating
     public String getPasswordStrengthRating() {
